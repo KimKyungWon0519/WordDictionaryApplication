fa5ca07b5924626ff5534987c60e59140050d308
KimKyungWon0519 <gwon080870@gmail.com>
2024-03-12 15:52:40 +0900

    FEAT: Create Field and Category

---
+++ app/src/main/java/app/kkw/worddictionaryapplication/AddWordActivity.kt
@@ -0,0 +1,29 @@
+package app.kkw.worddictionaryapplication
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class AddWordActivity : AppCompatActivity() {
+    private val menuItem = listOf("전체", "영어")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_word)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val menu: TextInputLayout = findViewById(R.id.menu)
+        val adapterItem = ArrayAdapter<String>(this, R.layout.menu_item, menuItem)
+        (menu.editText as? AutoCompleteTextView)?.setAdapter(adapterItem)
+    }
+}
