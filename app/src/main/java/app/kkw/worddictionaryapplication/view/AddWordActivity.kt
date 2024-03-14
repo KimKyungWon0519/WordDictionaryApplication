32dd3523ea6b161f8d5fda907ff849bcf6bb5c39
KimKyungWon0519 <gwon080870@gmail.com>
2024-03-14 19:54:21 +0900

    FEAT: Get editText.text on click search field

--- app/src/main/java/app/kkw/worddictionaryapplication/view/AddWordActivity.kt
+++ app/src/main/java/app/kkw/worddictionaryapplication/view/AddWordActivity.kt
@@ -1,8 +1,12 @@
 package app.kkw.worddictionaryapplication.view
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,7 +27,19 @@ class AddWordActivity : AppCompatActivity() {
         }
 
         val menu: TextInputLayout = findViewById(R.id.menu)
-        val adapterItem = ArrayAdapter<String>(this, R.layout.menu_item, menuItem)
+        val adapterItem = ArrayAdapter(this, R.layout.menu_item, menuItem)
         (menu.editText as? AutoCompleteTextView)?.setAdapter(adapterItem)
+
+        val searchButton: Button = findViewById(R.id.searchButton)
+
+        searchButton.setOnClickListener {
+            val inputMethodManager: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val textField: TextInputLayout = findViewById(R.id.searchField)
+
+            inputMethodManager.hideSoftInputFromWindow(textField.windowToken, 0)
+
+            Log.d("DEBUG", "field result : ${textField.editText?.text}")
+        }
     }
 }
